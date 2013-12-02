ac2935c933e3e4732f23b23e0d208500a6c5573d
torol <tor@torolofsson.se>
2013-12-02 13:15:33 +0100

    Starttest

---
+++ Pacman/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2 dec 2013
+ *      Author: tor
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Hej Pacman!";
+
+	return 0;
+}
