e75eced05b558958c74163ecb3f9530f7b748d5a
Akdas <akdho545@student.liu.se>
2013-12-11 17:38:38 +0100

    Ska mainfunktionen se ut såhär? Blir lättare att följa vad som kallas på vad om man vet var början är tänker jag.

--- Pacman/src/main.cpp
+++ Pacman/src/main.cpp
@@ -4,12 +4,23 @@
  *  Created on: 2 dec 2013
  *      Author: tor
  */
+/* Ska inte main se ut typ såhär? */
+
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	cout << "Hej Pacman!!!";
+
+	GameEngine* main = new GameEngine();
+
+	GameEngine->GameLoop();
+
+	delete main;
+
+
+	return 0;
+
 
 	return 0;
 }
