e6149f048528e82cc71e0ca9bcc3c5e4233d6805
eriknybom <erik.nybom@gmail.com>
2013-12-13 13:17:03 +0100

    PathFinder borde nu mer gora sitt jobb akdas-aka sweetiepie

--- Pacman/src/PathFinder.cpp
+++ Pacman/src/PathFinder.cpp
@@ -10,9 +10,89 @@
 #include <iostream>
 using namespace std;
 
-PathFinder::Direction getDirection(pointTarget)
+//Pathfinder tar in ghosts position och en target-position och retunerar en giltlig direction
+Direction PathFinder::getDirection(ghost_x,ghost_y, target_x, target_y)
 {
+Direction prevDirection = getDirection();
+int direction_x;
+int direction_y;
+direction_x = ghost_x - target_x;
+direction_y = ghost_y - target_y;
 
+if (direction_x >= direction_y && direction_x > 0)
+	{
+		if(prevDirection != LEFT && isWall(ghost_x -1, ghost_y)
+		{
+			setDirection(LEFT);
+		}
+		else if(direction_y > 0 && isWall(ghost_x, ghost_y -1) && prevDirection != DOWN)
+		{
+			setDirection(DOWN);
+		}
+		else if(isWall(ghost_x, ghost_y +1) && prevDirection != UP)
+		{
+			setDirection(UP);
+		}
+		else 
+		setDirection(RIGHT );
+	}
+if (direction_x >= direction_y && direction_x < 0)
+	{
+		if(prevDirection != RIGHT && isWall(ghost_x +1, ghost_y)
+		{
+			setDirection(prefDirection);
+		}
+		else if(direction_y > 0 && isWall(ghost_x, ghost_y -1) && prevDirection != DOWN)
+		{
+			setDirection(DOWN);
+		}
+		else if(isWall(ghost_x, ghost_y +1) && prevDirection != UP)
+		{
+			setDirection(UP);
+		}
+		else 
+		setDirection(LEFT);
+	}
+	
+if (direction_x <= direction_y && direction_y < 0)
+	{
+		if(prevDirection != UP && isWall(ghost_x, ghost_y + 1)
+		{
+			setDirection(UP);
+		}
+		else if(direction_x > 0 && isWall(ghost_x-1, ghost_y) && prevDirection != LEFT)
+		{
+			setDirection(LEFT);
+		}
+		else if(isWall(ghost_x + 1, ghost_y) && prevDirection != RIGHT)
+		{
+			setDirection(RIGHT);
+		}
+		else 
+		setDirection(DOWN);
+	}
+if (direction_x <= direction_y && direction_y > 0)
+	{
+		if(prevDirection != DOWN && isWall(ghost_x, ghost_y + 1)
+		{
+			setDirection(DOWN);
+		}
+		else if(direction_x > 0 && isWall(ghost_x-1, ghost_y) && prevDirection != LEFT)
+		{
+			setDirection(LEFT);
+		}
+		else if(isWall(ghost_x + 1, ghost_y) && prevDirection != RIGHT)
+		{
+			setDirection(RIGHT);
+		}
+		else 
+		setDirection(UP);
+	}
+	
+}
+
+
+/* TILL AICOMPONENT, PRONTO!
 int intDirection;
 random_device rd;
 mt19937 gen(rd());
@@ -30,4 +110,4 @@ return direction_ = UP;
 case(3)
 return direction_ = DOWN;
 }
-}
+*/
