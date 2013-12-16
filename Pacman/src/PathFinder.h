507cf4574622644e8769edb13ea882a4be31b59f
eriknybom <erik.nybom@gmail.com>
2013-12-16 14:16:09 +0100

    pathfinder.h tillbaka

---
+++ Pacman/src/PathFinder.h
@@ -0,0 +1,32 @@
+/**
+ * @file PathFinder.h
+ *
+ *  Created on: 6 dec 2013
+ * @author Erik N inte philip iallafall
+ */
+
+#ifndef PATHFINDER_H_
+#define PATHFINDER_H_
+
+
+#include "GameEngine.h"
+#include "Moveable.h"
+class Map;
+
+
+class PathFinder
+{
+public:
+Moveable::Direction getDirection(Moveable*, int, int);
+PathFinder(Map*);
+
+private:
+Map* internalMap;
+
+};
+
+
+
+
+
+#endif /* PATHFINDER_H_ */
